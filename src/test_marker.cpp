95eaaf54dffcb5155d3053b4c0510e169bbf9986
Adam Pelc <adam.pelc@apsoftware.icu>
2024-09-05 21:21:01 +0200

    Extended basic TCes

--- src/test_marker.cpp
+++ src/test_marker.cpp
@@ -1,8 +1,8 @@
 #include "test_marker.h"
 
 #define BOOST_STACKTRACE_LINK
-#include <boost/stacktrace.hpp>
 #include <boost/regex.hpp>
+#include <boost/stacktrace.hpp>
 
 namespace trick {
     std::optional<std::pair<std::string, std::string>> get_test_name() noexcept {
@@ -12,10 +12,9 @@ namespace trick {
         constexpr boost::match_flag_type flags = boost::match_not_dot_newline;
         const auto start = std::begin(stacktrace);
         const auto end = std::end(stacktrace);
-        while (boost::regex_search(start, end, what, expression, flags)) {
+        while (::boost::regex_search(start, end, what, expression, flags)) {
             return std::make_pair(what[1].str(), what[2].str());
         }
         return std::nullopt;
     }
-
 }
--- src/ut/test_marker.test.cpp
+++ src/ut/test_marker.test.cpp
@@ -4,12 +4,32 @@
 
 TEST(TestName, TestCaseName) {
     const auto [test_name, test_case_name] = trick::get_test_name().value();
-    ASSERT_EQ("TestName", test_name);
-    ASSERT_EQ("TestCaseName", test_case_name);
+    EXPECT_EQ("TestName", test_name);
+    EXPECT_EQ("TestCaseName", test_case_name);
 }
 
 TEST(TestName, TC12345_TestCaseName) {
     const auto [test_name, test_case_name] = trick::get_test_name().value();
-    ASSERT_EQ("TestName", test_name);
-    ASSERT_EQ("TC12345_TestCaseName", test_case_name);
+    EXPECT_EQ("TestName", test_name);
+    EXPECT_EQ("TC12345_TestCaseName", test_case_name);
 }
+
+class TestFixture : public ::testing::Test {};
+
+TEST_F(TestFixture, TestCaseName) {
+    const auto [test_name, test_case_name] = trick::get_test_name().value();
+    EXPECT_EQ("TestFixture", test_name);
+    EXPECT_EQ("TestCaseName", test_case_name);
+}
+
+class TestFixtureName : public ::testing::TestWithParam<int> {
+};
+
+TEST_P(TestFixtureName, TestName) {
+    const auto [test_name, test_case_name] = trick::get_test_name().value();
+    EXPECT_EQ("TestFixtureName", test_name);
+    EXPECT_EQ("TestName", test_case_name);
+}
+
+INSTANTIATE_TEST_CASE_P(Odd, TestFixtureName, ::testing::Values(1, 3, 5));
+INSTANTIATE_TEST_CASE_P(Even, TestFixtureName, ::testing::Values(2, 4, 6));
